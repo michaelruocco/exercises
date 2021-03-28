5d20a8fda4c17a7b07bdb5bae7fe7a2cda88fe60
Michael Ruocco <michael.ruocco@hotmail.com>
2021-03-28 09:33:55 +0100

    implements message conversion

--- instruction-processing/src/main/java/uk/co/mruoc/exercises/instructionprocessing/MessageConverter.java
+++ instruction-processing/src/main/java/uk/co/mruoc/exercises/instructionprocessing/MessageConverter.java
@@ -1,9 +1,18 @@
 package uk.co.mruoc.exercises.instructionprocessing;
 
+import java.time.Instant;
+
 public class MessageConverter {
 
     public InstructionMessage toInstructionMessage(String message) {
-        return InstructionMessage.builder().build();
+        String[] parts = message.split(" ");
+        return InstructionMessage.builder()
+                .type(InstructionType.valueOf(parts[1]))
+                .productCode(parts[2])
+                .quantity(Integer.parseInt(parts[3]))
+                .uom(Integer.parseInt(parts[4]))
+                .timestamp(Instant.parse(parts[5]))
+                .build();
     }
 
 }
