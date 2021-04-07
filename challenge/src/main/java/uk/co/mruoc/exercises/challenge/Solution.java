508c79fea9e7ee1bd4e3a161b59884882629c566
Michael Ruocco <michael.ruocco@hotmail.com>
2021-04-07 07:48:24 +0100

    more effecient solution

--- challenge/src/main/java/uk/co/mruoc/exercises/challenge/Solution.java
+++ challenge/src/main/java/uk/co/mruoc/exercises/challenge/Solution.java
@@ -9,20 +9,16 @@ public class Solution {
     public static int solution(int numberOfCakes, int requiredLayers, int[] starts, int[] ends, int[] flavours) {
         int[] lastLayers = new int[numberOfCakes];
         for (int f = 0; f < flavours.length; f++) {
-            for (int c = 0; c < numberOfCakes; c++) {
-                int cake = c + 1;
-                int lastLayer = lastLayers[c];
-                if (cake >= starts[f] && cake <= ends[f] && lastLayer != -1) {
-                    int flavour = flavours[f];
-                    if ((lastLayer + 1) == flavour) {
-                        lastLayers[c] = flavour;
-                    } else {
-                        lastLayers[c] = -1;
-                    }
+            for (int c = starts[f]; c <= ends[f]; c++) {
+                int index = c - 1;
+                int flavour = flavours[f];
+                if ((lastLayers[index] + 1) == flavour) {
+                    lastLayers[index] = flavour;
+                } else {
+                    lastLayers[index] = -1;
                 }
             }
         }
-
         int count = 0;
         for (int lastLayer : lastLayers) {
             if (lastLayer == requiredLayers) {
