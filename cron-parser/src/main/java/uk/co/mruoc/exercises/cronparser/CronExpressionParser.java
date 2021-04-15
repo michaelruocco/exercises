e44778673e43c16d5b8dedb437dde01e68af2242
Michael Ruocco <michael.ruocco@hotmail.com>
2021-04-15 20:12:05 +0100

    adds failing tests for initial functionality

---
+++ cron-parser/src/main/java/uk/co/mruoc/exercises/cronparser/CronExpressionParser.java
@@ -0,0 +1,9 @@
+package uk.co.mruoc.exercises.cronparser;
+
+public class CronExpressionParser {
+
+    public CronResult parse(String expression) {
+        return new CronResult();
+    }
+
+}
---
+++ cron-parser/src/main/java/uk/co/mruoc/exercises/cronparser/CronResult.java
@@ -0,0 +1,29 @@
+package uk.co.mruoc.exercises.cronparser;
+
+public class CronResult {
+
+    public int[] getMinutes() {
+        return new int[]{-1};
+    }
+
+    public int[] getHours() {
+        return new int[]{-1};
+    }
+
+    public int[] getDaysOfMonth() {
+        return new int[]{-1};
+    }
+
+    public int[] getMonths() {
+        return new int[]{-1};
+    }
+
+    public int[] getDayOfWeek() {
+        return new int[]{-1};
+    }
+
+    public String getCommand() {
+        return "";
+    }
+
+}
