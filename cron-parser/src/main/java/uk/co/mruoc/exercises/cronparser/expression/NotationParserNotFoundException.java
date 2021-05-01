64574f8fd1960f8b6a849b52a234a49aa318109a
Michael Ruocco <michael.ruocco@hotmail.com>
2021-05-01 07:11:39 +0100

    removes unused class and properties file

--- cron-parser/src/main/java/uk/co/mruoc/exercises/cronparser/expression/NotationParserNotFoundException.java
+++ cron-parser/src/main/java/uk/co/mruoc/exercises/cronparser/expression/NotationParserNotFoundException.java
@@ -7,4 +7,5 @@ public class NotationParserNotFoundException extends ParserException {
     public NotationParserNotFoundException(String value) {
         super(String.format("notation parser not found for value %s", value));
     }
+
 }
