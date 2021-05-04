9483a0f1dc9124b4b3bd3592eaeba5c56546466a
Michael Ruocco <michael.ruocco@hotmail.com>
2021-05-04 07:50:58 +0100

    removes constructor thats no longer used

--- cron-parser/src/main/java/uk/co/mruoc/exercises/cronparser/expression/notation/InvalidNotationException.java
+++ cron-parser/src/main/java/uk/co/mruoc/exercises/cronparser/expression/notation/InvalidNotationException.java
@@ -4,10 +4,6 @@ import uk.co.mruoc.exercises.cronparser.ParserException;
 
 public class InvalidNotationException extends ParserException {
 
-    public InvalidNotationException(String value) {
-        super(value);
-    }
-
     public InvalidNotationException(String value, Throwable cause) {
         super(value, cause);
     }
