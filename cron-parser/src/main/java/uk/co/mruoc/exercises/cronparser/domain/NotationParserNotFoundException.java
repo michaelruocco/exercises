efb3faffe0620440ef5183685795073665a281e0
Michael Ruocco <michael.ruocco@hotmail.com>
2021-05-05 08:12:19 +0100

    deletes cron parser now that its been extracted into its own repo

--- cron-parser/src/main/java/uk/co/mruoc/exercises/cronparser/domain/NotationParserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package uk.co.mruoc.exercises.cronparser.domain;
-
-public class NotationParserNotFoundException extends ParserException {
-
-    public NotationParserNotFoundException(String value) {
-        super(String.format("notation parser not found for value %s", value));
-    }
-
-}
--- cron-parser/src/main/java/uk/co/mruoc/exercises/cronparser/domain/Writer.java
+++
@@ -1,9 +0,0 @@
-package uk.co.mruoc.exercises.cronparser.domain;
-
-public interface Writer {
-
-    void writeOutput(String value);
-
-    void writeError(String value);
-
-}
