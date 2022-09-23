66659a711ee86880fe69b46b6186ec9f7dbd039c
Michael Ruocco <michael.ruocco@hotmail.com>
2022-09-23 07:59:16 +0100

    updates dependencies

--- palindrome-app/src/test/java/uk/co/mruoc/exercises/palindrome/domain/PalindromeFinderTest.java
+++ palindrome-app/src/test/java/uk/co/mruoc/exercises/palindrome/domain/PalindromeFinderTest.java
@@ -1,13 +1,10 @@
 package uk.co.mruoc.exercises.palindrome.domain;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,11 +13,17 @@ class PalindromeFinderTest {
     private final PalindromeFinder finder = new PalindromeFinder();
 
     @ParameterizedTest
-    @MethodSource("inputsAndPalindromes")
-    void shouldReturnAllPalindromesIfInputContainsMultiplePalindromes(String input, Collection<String> expectedPalindromes) {
+    @CsvSource(value = {
+            "racecar: racecar",
+            "was it a rat i saw: wasitaratisaw",
+            "palindromes level phrase: level",
+            "anna & level: level,anna",
+            "anna & level and racecar: racecar,level,anna"
+    }, delimiter = ':')
+    void shouldReturnAllPalindromesIfInputContainsMultiplePalindromes(String input, String expectedPalindromes) {
         Collection<String> palindromes = finder.findPalindromes(input);
 
-        assertThat(palindromes).containsExactlyElementsOf(expectedPalindromes);
+        assertThat(palindromes).containsExactly(expectedPalindromes.split(","));
     }
 
     @ParameterizedTest
@@ -33,14 +36,5 @@ class PalindromeFinderTest {
         Collection<String> palindromes = finder.findPalindromes(input);
 
         assertThat(palindromes).isEmpty();
-    }
-
-    private static Stream<Arguments> inputsAndPalindromes() {
-        return Stream.of(
-                Arguments.of("racecar", List.of("racecar")),
-                Arguments.of("was it a rat i saw", List.of("wasitaratisaw")),
-                Arguments.of("was it your palindrome, level with me was it really?", List.of("level")),
-                Arguments.of("anna & level", List.of("level", "anna"))
-        );
     }
 }
